ed1e28fd77437d6b214a01ed60fc3cc219906afc
Rohit kumar <rohitprj.1282@gmail.com>
2024-05-15 21:34:19 +0530

    project

---
+++ clone/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import screen1 from './pages/screen1.js';
+
+function App() {
+  return
+  (
+    <screen1 />
+  )
+}
+
+export default App;
---
+++ clone/src/pages/screen1.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function screen1() {
+
+    return <h1>hello world</h1>
+}
+export default screen1;
